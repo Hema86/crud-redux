f4874f719455d1ef8e82c441678fad1e607aadef
Hema86 <hemakempur@gmail.com>
2019-07-24 22:42:25 +0530

    added add and delete

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react'
-import './App.css'
+import PostForm from './PostForm'
+import AllPost from './AllPost'
 
 class App extends Component {
   render () {
     return (
       <div className='App'>
-        <h1>Hello</h1>
+        <PostForm />
+        <AllPost />
       </div>
     )
   }
 }
-
 export default App
